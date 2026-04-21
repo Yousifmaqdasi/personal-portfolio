d230d0406b88524fa0f78c3bc2c19caa333914d9
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-22 00:55:53 +0200

    chore(app): add state for NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,11 +11,15 @@ import {
 } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const NavBar = () => {
+
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <nav className="p-4">
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger asChild className="border-none">
           <Button variant={"ghost"} className="w-10 h-10">
             <Menu className="!w-5.5 !h-5.5"/>
@@ -29,13 +33,13 @@ const NavBar = () => {
           </SheetHeader>
 
           <div className="flex flex-col gap-8 px-4 py-2">
-            <Link href="/" className="block">
+            <Link href="/" className="block" onClick={() => setIsOpen(false)}>
               Home
             </Link>
-            <Link href="/my-projects" className="block">
+            <Link href="/my-projects" className="block" onClick={() => setIsOpen(false)}>
               Projects
             </Link>
-            <Link href="/contact" className="block">
+            <Link href="/contact" className="block" onClick={() => setIsOpen(false)}>
               Contact
             </Link>
           </div>
